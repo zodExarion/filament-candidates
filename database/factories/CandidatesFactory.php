4cd442c6b40ba84faefecf3633b42959dce7eb2d
mcdg055 <deguzmanmartinc@gmail.com>
2024-05-01 12:42:20 +0800

    fix seeder

--- database/factories/CandidatesFactory.php
+++ database/factories/CandidatesFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\Candidates;
+use App\Models\Language;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -15,22 +16,34 @@ class CandidatesFactory extends Factory
      *
      * @return array<string, mixed>
      */
-    
+
     public function definition(): array
     {
+        //generate random array element from given array
+
         return [
             'email' => $this->faker->unique()->safeEmail,
+            'telephone_number' => $this->faker->phoneNumber,
             'phone_number' => $this->faker->phoneNumber,
             'first_name' => $this->faker->firstName,
             'last_name' => $this->faker->lastName,
             'sex' => $this->faker->randomElement(['male', 'female']),
             'date_of_birth' => $this->faker->date(),
             'position' => $this->faker->jobTitle,
-            'languages' => $this->faker->randomElement([2, 1]),
+            'languages' => $this->faker->randomElements(Language::pluck('id')->toArray(), Language::count()),
             'driving_license' => null, // For now, as it's nullable
             'cv' => null, // For now, as it's nullable
             'own_transport' => $this->faker->boolean,
             'is_working' => $this->faker->randomElement([0, 1, 2, 3]), // Randomly assign is_working value
+            'projects'=>[
+                [
+                    'title' => $this->faker->sentence,
+                    'description' => $this->faker->sentence,
+                    'url' => $this->faker->url,
+                    'start_date' => $this->faker->date(),
+                    'end_date' => $this->faker->date(),
+                ]
+            ]
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use App\Models\Candidates;
 
@@ -13,14 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([LanguagesSeeder::class]);
 
-        \App\Models\User::factory()->create([
+        User::factory()->create([
             'name' => 'admin',
             'email' => 'admin1@admin.com',
         ]);
-        Candidates::factory()->count(50)->create();
 
-        $this->call([LanguagesSeeder::class]);
+        Candidates::factory()->count(50)->create();
     }
 }
